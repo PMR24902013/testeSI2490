b3323ade60a4641cfff229aa4cf7c6de00aaa069
Flávio <Flvio@Flvio-PC>
2013-10-26 12:16:07 -0200

    Criacao do Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Flávio
+ */
+
+public class HelloWorld {
+    
+    public static void HelloWorld(void){
+        System.out.println("Hello, pretty world");
+    }
+}
